6ad104b6ba6c31f50df1bb4846db90947cf72470
Sławomir Piwowar <slawomir.piwowar@dreamlab.pl>
2017-09-13 00:06:36 +0200

    testing

---
+++ src/Element/Password.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ms1570p\ZendFormElementDiscovery\Element;
+
+/**
+ * Class Select
+ *
+ * @package ms1570p\FormElementDiscovery\Element
+ */
+class Password extends Base
+{
+    /**
+     * @var string
+     */
+    protected $elementClass = \Zend_Form_Element_Password::class;
+}
